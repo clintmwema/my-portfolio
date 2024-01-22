606dc34077a4a21f1b0e0eef8b9e29cfca6865d1
Clinton Rori <clintmwema@gmail.com>
2024-01-22 10:01:48 +0300

    configure esclintrc

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
         <section>
             <div className='grid grid-cols-1 lg:grid-cols-12'>    
                 <div className='col-span-7 place-self-center'>           
-                    <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hi, I'm Rori</h1>
+                    <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hi, I&apos;m Rori</h1>
                     <p className='text-white text-lg lg:text-xl'>
                         Intermediate in full-stack development, advanced computer user,
                         and currently exploring Web3 and blockchain development.
